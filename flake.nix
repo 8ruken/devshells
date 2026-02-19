832030164a1e1f3a8bd0ea82894ea2f5e3441e0d
8ruken <kensei.koyama.3g@stu.hosei.ac.jp>
2026-02-19 22:15:26 +0900

    first commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Rust dev env (WSL + Nix)";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      fenix,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          dir = ./.;
+          sha256 = "sha256-SBKjxhC6zHTu0SyJwxLlQHItzMzYZ71VCWQC2hOzpRY=";
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            toolchain
+            fenix.packages.${system}.rust-analyzer
+            pkgs.git
+            pkgs.nixfmt
+          ];
+        };
+      }
+    );
+}
